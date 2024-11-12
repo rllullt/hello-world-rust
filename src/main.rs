b2dc42b8d048f19dedaf908991490a6ff76e91e0
Rodrigo Llull Torres <rllullt@gmail.com>
2024-11-12 20:47:35 -0300

    Option type

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
 fn main() {
-    println!("Hello, world!");
+    let mut message = String::from("Name: Óscar, Height: ");
+    message.clear();
+    let mut height = 190;
+    height = 189;
+    println!("{}{}", message, height);
+
+    el_maybe();
+}
+
+fn el_maybe() {
+    let maybe_number = Some(42);
+    if let Some(number) = maybe_number {
+        println!("The number is: {:?}", number);
+    }
+    else {
+        println!("There is no number");
+    }
 }
