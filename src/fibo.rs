02fb781a83392223b07a0de1f18df92e8efa1a53
Rodrigo Llull Torres <rllullt@gmail.com>
2025-06-18 20:19:00 -0400

    One line function calculation with an if expression

--- src/fibo.rs
+++ src/fibo.rs
@@ -1,7 +1,8 @@
 pub fn fibo(n: u32) -> u32 {
     // Caso base
-    if n == 0 || n == 1 {  // no son necesarios los paréntesis en este `if`
-        return n;
-    }
-    fibo(n-1) + fibo(n-2)
+//    if n == 0 || n == 1 {  // no son necesarios los paréntesis en este `if`
+//        return n;
+//    }
+//    fibo(n-1) + fibo(n-2)
+    if n == 0 || n == 1 { n } else { fibo(n-1) + fibo(n-2) }
 }
